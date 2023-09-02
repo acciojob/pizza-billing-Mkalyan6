d386f4fb4732f5b397f81b591ed546ee7dd60a03
Mkalyan6 <120394363+Mkalyan6@users.noreply.github.com>
2023-09-02 16:10:17 +0530

    Pizza billing application

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,8 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        // when ever the customer selects deluxe pizza, then by default  ho selected cheese and toppings
+        addExtraCheese();
+        addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,83 @@
 package com.driver;
 
-public class Pizza {
+public class  Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
+    protected Boolean isExtraCheese = false;
+    protected Boolean isExtraTopping = false;
 
-    public Pizza(Boolean isVeg){
+    protected Boolean BagOrdered = false;
+    protected Boolean IsBillGen = false;
+
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
         // your code goes here
+        if (isVeg) price = 300;
+        else price = 400;
+
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
+    public void addExtraCheese() {
         // your code goes here
+        if (!isExtraCheese) {
+            price += 80;
+            isExtraCheese = true;
+        }
     }
 
-    public void addExtraToppings(){
+    public void addExtraToppings() {
         // your code goes here
+        if (!isExtraTopping) {
+            if (isVeg) {
+                price += 70;
+            } else {
+                price += 120;
+            }
+            isExtraTopping = true;
+        }
+
     }
 
-    public void addTakeaway(){
+    public void addTakeaway() {
         // your code goes here
+        if (!BagOrdered) {
+            price += 20;
+            BagOrdered = true;
+        }
     }
 
-    public String getBill(){
+    public String getBill() {
         // your code goes here
-        return this.bill;
+        if(IsBillGen) return bill;
+
+            // first check for veg or not
+            if (isVeg) {
+                System.out.println("Base Price Of The Pizza: 300");
+            } else {
+                System.out.println("Base Price Of The Pizza: 400");
+            }
+           if(isExtraCheese){
+               System.out.println("Extra Cheese Added: 80");
+           }
+           if(isExtraTopping){
+               if(isVeg){
+                   System.out.println("Extra Toppings Added: 70");
+               }else{
+                   System.out.println("Extra Toppings Added: 120");
+               }
+           }
+           if(BagOrdered){
+               System.out.println("Paperbag Added: 20");
+           }
+           IsBillGen=true;
+           return bill="Total Price:" +price;
     }
+
 }
+
